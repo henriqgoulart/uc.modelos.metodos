a81c4067202574e0049dde98a34b5526aaabea89
Henrique Goulart Inácio <104533964+henriqgoulart@users.noreply.github.com>
2024-06-27 19:51:47 -0300

    Create ActivityScreen.js

---
+++ ActivityScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+export default function ActivityScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Add Activity</Text>
+      <TextInput style={styles.input} placeholder="Activity Name" />
+      <TextInput style={
+
