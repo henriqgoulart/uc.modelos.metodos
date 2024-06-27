e7c3eccc276bc34fb4cbc16cc8f4e5efb3ad3d61
Henrique Goulart Inácio <104533964+henriqgoulart@users.noreply.github.com>
2024-06-27 19:50:02 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import DashboardScreen from './src/screens/DashboardScreen';
+import ActivityScreen from './src/screens/ActivityScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Activity" component={ActivityScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
