c86b07ffb65858b675834fb0343e1da3675582c4
Henrique Goulart Inácio <104533964+henriqgoulart@users.noreply.github.com>
2024-06-27 19:38:56 -0300

    Create MyHealthApplication.java

---
+++ MyHealthApplication.java
@@ -0,0 +1,11 @@
+package com.myhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHealthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyHealthApplication.class, args);
+    }
+}
